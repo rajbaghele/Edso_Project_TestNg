0cc6d67c93038ab3d70e632bb5b47ef679357b83
rajbaghele <162471827+rajbaghele@users.noreply.github.com>
2024-03-06 23:54:46 +0530

    Add files via upload

---
+++ src/test/java/annotation/WithoutPriority.java
@@ -0,0 +1,34 @@
+package annotation;
+
+import org.testng.annotations.Test;
+
+public class WithoutPriority {
+    @Test
+    public void tC_A(){
+        System.out.println("Test case A");
+    }
+    @Test
+    public void tC_B(){
+        System.out.println("Test case B");
+    }
+    @Test
+    public void tC_C(){
+        System.out.println("Test case C");
+    }
+    @Test
+    public void tC_D(){
+        System.out.println("Test case D");
+    }
+    @Test
+    public void tC_E(){
+        System.out.println("Test case E");
+    }
+    @Test
+    public void tC_F(){
+        System.out.println("Test case F");
+    }
+    @Test
+    public void tC_G(){
+        System.out.println("Test case G");
+    }
+}
